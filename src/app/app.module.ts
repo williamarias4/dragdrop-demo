3bffe4aec055247ff53563175fdd33dfff270585
“William <warias@wfscorp.com>
2023-07-15 02:44:55 -0600

    Added Index for scss base importing.
    Added standarized button styling.
    
    Added dashboard component for drago and drop.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DragDropComponent
+    DragDropComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
